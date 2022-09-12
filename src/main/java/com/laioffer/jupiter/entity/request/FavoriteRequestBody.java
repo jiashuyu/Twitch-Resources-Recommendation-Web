0719e2f46f059797645449a007d9ae339aab8f1d
Shuyu Jia <shuyuj2@illinois.edu>
2022-09-12 11:03:11 +0800

    My first commit

---
+++ src/main/java/com/laioffer/jupiter/entity/request/FavoriteRequestBody.java
@@ -0,0 +1,14 @@
+package com.laioffer.jupiter.entity.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.laioffer.jupiter.entity.db.Item;
+
+public class FavoriteRequestBody {
+
+    @JsonProperty("favorite")
+    private Item favoriteItem;
+
+    public Item getFavoriteItem() {
+        return favoriteItem;
+    }
+}
